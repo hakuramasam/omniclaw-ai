a5d04e6523540a0d5ccfd9535e67fba32d06482a
HakuramaSam <86550572+hakuramasam@users.noreply.github.com>
2026-03-10 17:19:12 +0630

    Create src/config/env.ts

---
+++ backend/src/config/env.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const ENV = {
+
+ openai:process.env.OPENAI_KEY!,
+ baseRpc:process.env.BASE_RPC!,
+ monadRpc:process.env.MONAD_RPC!,
+ privateKey:process.env.AGENT_PRIVATE_KEY!,
+ port:process.env.PORT || 3001
+
+}
